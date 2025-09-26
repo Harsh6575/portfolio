0b2a5d451179a666e10d0a5d8a009f79ea4208c0
“Harsh6575” <harsh.190410107146@gmail.com>
2025-09-26 17:11:54 +0530

    loading added and react icons installed

---
+++ components/loader.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { BounceLoader } from "react-spinners";
+
+export const Loader = () => {
+    return (
+      <div className="h-full flex items-center justify-center">
+        <BounceLoader color="#2b7fff" size={40} />
+      </div>
+    );
+  };
