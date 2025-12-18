7789d83d090c7046950dc040075e3cfe486ef1e7
“Harsh6575” <harsh.190410107146@gmail.com>
2025-12-18 17:47:52 +0530

    feat: turn off openToWork

--- constants/index.ts
+++ constants/index.ts
@@ -2,5 +2,5 @@ export const MAIL_URL: string = "mailto:harshvansjaliya3@gmail.com";
 export const LINKED_IN_URL: string = "https://linkedin.com/in/harsh-vansjaliya-904825226/";
 export const GITHUB_URL: string = "https://github.com/harsh6575";
 export const X_URL: string = "https://x.com/HarshVansjaliya";
-export const OPEN_TO_NEW_OP: boolean = true;
+export const OPEN_TO_NEW_OP: boolean = false;
 export const TOTAL_EXPERIENCE: number = new Date().getFullYear() - 2023;
